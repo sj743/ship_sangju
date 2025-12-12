4e741f4f67fd71137cbc4757283612c4bf2bf493
이상주 <sang.j0743@gmail.com>
2025-12-12 09:47:34 +0900

    config edit

--- playwright.config.js
+++ playwright.config.js
@@ -5,17 +5,13 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   use: {
-    storageState: undefined, // 세션 공유 방지
+    storageState: undefined,
     screenshot: 'only-on-failure',
     video: {
       mode: 'on',
-      dir: 'tests/video',
-      size: { width: 1920, height: 1080 } // 영상 해상도
+      dir: 'tests/video'
     },
-    viewport: { width: 1920, height: 1080 }, // 브라우저 창 크기 통일
-    launchOptions: {
-      args: ['--start-maximized'], // 실제 브라우저 창 최대화
-    },
+    viewport: { width: 1280, height: 720 }, // ★ 16:9 기준
   },
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
 });
